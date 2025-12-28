ce3b5583bf7aa2d16c35883aa3bf75a115b7131a
Luhuss <l.maraboliq@gmail.com>
2025-12-28 17:22:07 -0300

    Activando PostgreSQL y puerta trasera para admin

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url  # <--- AGREGAR ESTO AL PRINCIPIO
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,11 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Configuración de Base de Datos Híbrida
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Si estamos en Render, usa PostgreSQL. Si estamos en tu Mac, usa SQLite.
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required # con eso se importa un candado de seguridad
 from .models import Cliente
 
+# --- AGREGA ESTAS IMPORTACIONES AL PRINCIPIO ---
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 @login_required
 def lista_clientes(request):
     # comenzamos consultando la base de datos (select * from Cliente)
@@ -10,4 +14,13 @@ def lista_clientes(request):
     # Enviamos los datos al html
     return render(request, 'core/lista_clientes.html', {'clientes': clientes})
 
+# --- AGREGA ESTA FUNCIÓN AL FINAL DEL ARCHIVO ---
+def crear_admin_rapido(request):
+    # Verificamos si ya existe para no crear duplicados
+    if not User.objects.filter(username='admin').exists():
+        # Crea el usuario: admin / correo@ejemplo.com / clave: admin123
+        User.objects.create_superuser('admin', 'admin@ejemplo.com', 'admin123')
+        return HttpResponse("¡Listo! Superusuario 'admin' creado con clave 'admin123' en PostgreSQL.")
+    else:
+        return HttpResponse("El usuario 'admin' ya existe. Intenta loguearte.")
 
