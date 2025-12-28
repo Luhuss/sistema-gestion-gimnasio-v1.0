8c94e9a0bf12d1a35b4abffec31fc012810fc8e1
Luhuss <l.maraboliq@gmail.com>
2025-12-28 15:09:12 -0300

    Cerrando puerta trasera de seguridad - Deploy Final

--- core/views.py
+++ core/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required # con eso se importa un candado de seguridad
 from .models import Cliente
-from django.http import HttpResponse
-from django.contrib.auth.models import User
 
 @login_required
 def lista_clientes(request):
@@ -12,12 +10,4 @@ def lista_clientes(request):
     # Enviamos los datos al html
     return render(request, 'core/lista_clientes.html', {'clientes': clientes})
 
-def crear_admin_rapido(request):
-    # Verificamos si ya existe para no crear duplicados
-    if not User.objects.filter(username='admin').exists():
-        # Crea el usuario: admin / correo@ejemplo.com / clave: admin123
-        User.objects.create_superuser('admin', 'admin@ejemplo.com', 'admin123')
-        return HttpResponse("¡Listo! Superusuario 'admin' creado con clave 'admin123'.")
-    else:
-        return HttpResponse("El usuario 'admin' ya existe. Intenta loguearte.")
 
